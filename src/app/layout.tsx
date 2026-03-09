797923fc0c23133a142960da1f49fc2e177354fa
xeanoo <boutrinambroise@gmail.com>
2026-03-09 16:10:50 +0100

    correction favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -165,7 +165,7 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
 
-        <div className="bg-noise opacity-20"></div>
+
 
 
         <Providers>
